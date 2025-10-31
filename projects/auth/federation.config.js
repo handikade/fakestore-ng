59e1d7c41d7d1f1ab93df5ccebef2d5ad99f3cab
handikade <dika.csui@gmail.com>
2025-10-31 23:19:27 +0700

    build(federation-config): share http

--- projects/auth/federation.config.js
+++ projects/auth/federation.config.js
@@ -10,6 +10,7 @@ module.exports = withNativeFederation({
 
   shared: {
     ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+    '@angular/common/http': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
   },
 
   skip: [
